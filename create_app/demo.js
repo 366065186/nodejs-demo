12335998924bd1dc80c27db26a15a9ef648afd9c
zhanghui <zhanghui@pkcms.cn>
2016-07-22 08:59:04 +0800

    start

---
+++ create_app/demo.js
@@ -0,0 +1,13 @@
+// 引入 require 模块
+var http = require('http');
+// 创建服务器
+http.createServer(function(request,response){
+	// 发送 HTTP 头部 
+	// HTTP 状态值: 200 : OK
+	// 内容类型: text/plain
+	response.writeHead(200, {'Content-Type':'text/plain'});
+	// 发送响应数据 "Hello World"
+	response.end('Hello World!\n');
+}).listen(2337, '127.0.0.1');
+// 终端打印如下信息
+console.log('Server running at http://127.0.0.1:2337/');
